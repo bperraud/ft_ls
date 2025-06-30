e0e21ad96762294337cc4493a295970896dbd84d
bperraud <74benji@live.fr>
2025-07-01 00:36:54 +0200

    handle file instead of dir

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sys/stat.h>
+
 
 typedef enum {
     FORMAT_LONG,
@@ -35,8 +37,52 @@ void sort_ascii(struct dirent *entries[], size_t count) {
     }
 }
 
+int is_regular_file(const char *path) {
+    struct stat st;
+    if (stat(path, &st) == 0) {
+        return S_ISREG(st.st_mode);
+    } else {
+        perror("stat");
+        return 0;
+    }
+}
+
+const char* print_basename(const char *path) {
+    const char *base = strrchr(path, '/');
+    if (base) {
+        return base + 1;
+    } else {
+        return path;
+    }
+}
+
+void print_regular_file(const char *path) {
+    path = print_basename(path);
+    if (is_regular_file(path)) {
+        printf("%s\n", path);
+    }
+    else {
+        printf("dir : %s\n", path);
+    }
+}
+
+char *concat(const char *s1, const char *s2) {
+    size_t len1 = strlen(s1);
+    size_t len2 = strlen(s2);
+    char *result = malloc(len1 + len2 + 1);
+    if (!result) return NULL;
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
 
 int ft_ls(char *path, _options options) {
+    if (is_regular_file(path)) {
+        print_regular_file(path);
+        return 0;
+    }
+
     DIR *dir = opendir(path);
 
     if (!dir) {
@@ -75,12 +121,16 @@ int ft_ls(char *path, _options options) {
 
     // Print in reverse
     for (ssize_t i = entries_number - 1; i >= 0; i--) {
-        printf("%s\n", entries[i]->d_name);
+        // printf("%s\n", );
+        char *complete_path = concat(path, entries[i]->d_name);
+        print_regular_file(complete_path);
         free(entries[i]);
+        free(complete_path);
     }
 
     return 0;
 }
+
 
 int start(int argc, char **argv) {
     output_format_t format;
